0f9cbbe00618bc8a66ee9d49807b1b49113ed446
joshjung <joshua.p.jung@gmail.com>
2015-09-25 12:42:46 -0500

    Updates to get embers integrated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,23 +35,15 @@ $(document).scroll(function () {
 var canvas = document.getElementById('canvasOverlay');
 
 var embers = new Embers(canvas, {
-  colors: ['#D6BD9F', '#B77F49', '#EFD7BF', '#A44C2C', '#D2A383']
+  debug: true,
+  colors: ['#D6BD9F', '#B77F49', '#EFD7BF', '#A44C2C', '#D2A383'],
+  sparkSource: {
+    target: document.getElementById('campfire'),
+    widthProp: 'clientWidth',
+    heightProp: 'clientHeight',
+    offset: {
+      x: '50%',
+      y: '60%'
+    }
+  }
 });
-
-// var ctx = canvas.getContext('2d');
-
-// canvas.addEventListener('resize', canvasOverlayResize.bind(canvas));
-
-// function canvasOverlayResize() {
-//   this.style.width = window.innerWidth;
-//   this.style.height = window.innerHeight;
-
-//   this.setAttribute('width', window.innerWidth);
-//   this.setAttribute('height', window.innerHeight);
-
-//   ctx.reset
-//   ctx.fillStyle = 'red';
-//   ctx.fillRect(0, 0, 100, 100);
-// }
-
-// canvasOverlayResize.call(canvas);
