9068c6122f41be94db4bf2027df047a75432ed01
joshjung <joshua.p.jung@gmail.com>
2015-09-25 08:45:13 -0500

    Adding in the embers.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,9 @@
 /*global $*/
 'use strict';
 
+/*---------------------------------------------------------------------------*\
+ * Scroll-to-bottom
+\*---------------------------------------------------------------------------*/
 var setScrollPosition = function () {
     window.setTimeout(function (){
       $('html, body').scrollTop( $(document).height() );
@@ -25,3 +28,30 @@ $(document).scroll(function () {
     divScrollUp.removeClass('scroll-up-show');
   }
 });
+
+/*---------------------------------------------------------------------------*\
+ * Canvas Overlay
+\*---------------------------------------------------------------------------*/
+var canvas = document.getElementById('canvasOverlay');
+
+var embers = new Embers(canvas, {
+  colors: ['#D6BD9F', '#B77F49', '#EFD7BF', '#A44C2C', '#D2A383']
+});
+
+// var ctx = canvas.getContext('2d');
+
+// canvas.addEventListener('resize', canvasOverlayResize.bind(canvas));
+
+// function canvasOverlayResize() {
+//   this.style.width = window.innerWidth;
+//   this.style.height = window.innerHeight;
+
+//   this.setAttribute('width', window.innerWidth);
+//   this.setAttribute('height', window.innerHeight);
+
+//   ctx.reset
+//   ctx.fillStyle = 'red';
+//   ctx.fillRect(0, 0, 100, 100);
+// }
+
+// canvasOverlayResize.call(canvas);
