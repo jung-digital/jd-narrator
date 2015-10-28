dffc34c8b6d265d2333a2b680e6dc2dbc1c0f25e
joshjung <joshua.p.jung@gmail.com>
2015-10-28 08:30:59 -0500

    Fix website load to be smoother.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,52 +7,18 @@
 window.CAMPFIRE_SCROLL_RATIO = 1 / 1.61;
 
 /*---------------------------------------------------------------------------*\
- * Scroll-to-bottom
-\*---------------------------------------------------------------------------*/
-var setScrollPosition = function () {
-    window.setTimeout(function (){
-      $('html, body').scrollTop( $(document).height() );
-    }, 10);
-    $('body').css('animation-play-state', 'running');
-};
-
-$('html, body').scrollTop( $(document).height() );
-$(window).load(setScrollPosition);
-
-$('body').on('animationend', function() {
-  $(this).css('opacity', 1);
-});
-
-$(document).scroll(function () {
-  var divScrollUp = $('.scroll-up');
-  var divSocialIcons = $('.social-fixed');
-  var divNarratorLogo = $('.narrator-logo-full');
-
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    divScrollUp.addClass('scroll-up-show');
-    divSocialIcons.addClass('social-show');
-    divNarratorLogo.addClass('narrator-logo-show');
-  }
-  else {
-    divScrollUp.removeClass('scroll-up-show');
-    divSocialIcons.removeClass('social-show');
-    divNarratorLogo.removeClass('narrator-logo-show');
-  }
-});
-
-/*---------------------------------------------------------------------------*\
- * Canvas Overlay
+ * Ember Canvas Overlay
 \*---------------------------------------------------------------------------*/
 var canvas = document.getElementById('embers');
 
 window.emberOptions = {
-  sparkCount: 100,
+  sparkCount: 90,
   maxSparkSize: 2.5,
   minSparkSize: 1.0,
   maxSparkVelocity: 120,
   minSparkVelocity: 50,
   maxSparkLife: 25,
-  maxTailLength: 19,
+  maxTailLength: 18,
   sparkSource: {
     target: document.getElementById('campfire-video'),
     widthProp: 'clientWidth',
@@ -65,27 +31,32 @@ window.emberOptions = {
   scrollRatio: window.CAMPFIRE_SCROLL_RATIO
 };
 
-var renderer = new Jungle.GraphicRenderer(canvas, {
+window.emberRenderer = new Jungle.GraphicRenderer(canvas, {
     canvasAutoClear: undefined,
     debug: true,
-    debugPosX: 10,
-    debugPosY: 70
+    debugPosX: 500,
+    debugPosY: 100,
+    fillWindowWidth: true
   }, 'embers');
 
-renderer.addChild(new Embers(window.emberOptions));
+emberRenderer.addChild(new Embers(window.emberOptions));
 
+/*---------------------------------------------------------------------------*\
+ * Ember Canvas Underlay
+ \*---------------------------------------------------------------------------*/
 canvas = document.getElementById('stars');
 
-renderer = new Jungle.GraphicRenderer(canvas, {
+window.starRenderer = new Jungle.GraphicRenderer(canvas, {
   canvasAutoClear: undefined,
   debug: true,
   debugPosX: 10,
-  debugPosY: 70
+  debugPosY: 100,
+  fillWindowWidth: true
 }, 'stars');
 
-renderer.addChild(new Starfield({
+starRenderer.addChild(new Starfield({
   starViewScrollRatio: 0.1,
-  starDensity: 5,
+  starDensity: 2,
   starViewWidth: 1920,
   starViewHeight: 9000
 }));
--- app/scripts/parallax.js
+++ app/scripts/parallax.js
@@ -1,4 +1,4 @@
-/*global $,ScrollMagic,TweenLite,Power3,Power1*/
+/*global $,ScrollMagic,TweenLite,Power3,Power1,Sine*/
 'use strict';
 
 /*------------------------------------------------------------------------------------*\
@@ -15,7 +15,7 @@ function addEnterLeaveTransition(_scene, element, top) {
         top: top,
         opacity: 1,
         overwrite: 'concurrent',
-        ease: Power3.easeInOut
+        ease: Power1.easeOut
       });
   });
 
@@ -24,7 +24,7 @@ function addEnterLeaveTransition(_scene, element, top) {
         top: event.scrollDirection === 'REVERSE' ? '200%' : '-200%',
         opacity: 0,
         overwrite: 'concurrent',
-        ease: Power3.easeInOut
+        ease: Power1.easeInOut
       });
   });
 }
@@ -43,12 +43,11 @@ var controller = new ScrollMagic.Controller({
 /*-----------------------------------------------------------*\
  * Section Globals
 \*-----------------------------------------------------------*/
-
 function getTopPercent(mobile, desktop) {
   if ($(window).width() < 785) {
     if (mobile) {
       return mobile;
-    }    
+    }
     return '5%';
   }
   else {
@@ -59,101 +58,148 @@ function getTopPercent(mobile, desktop) {
   }
 }
 
-//-------------------------------------
-// section-contact
-//-------------------------------------
-var sceneContact = new ScrollMagic.Scene({
-  triggerElement: '#section-contact-trigger',
-  duration: $('#section-contact-trigger').height()
+function setupScenes() {
+  //-------------------------------------
+  // section-contact
+  //-------------------------------------
+  var sceneContact = new ScrollMagic.Scene({
+    triggerElement: '#section-contact-trigger',
+    duration: $('#section-contact-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneContact, '#section-contact-child', getTopPercent());
+
+  sceneContact.addTo(controller);
+
+  //-------------------------------------
+  // section-story-type
+  //-------------------------------------
+  var sceneStoryType = new ScrollMagic.Scene({
+    triggerElement: '#section-story-type-trigger',
+    duration: $('#section-story-type-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneStoryType, '#section-story-type-child', getTopPercent());
+
+  sceneStoryType.addTo(controller);
+
+  //-------------------------------------
+  // section-workshop
+  //-------------------------------------
+  var sceneWorkshop = new ScrollMagic.Scene({
+    triggerElement: '#section-workshop-trigger',
+    duration: $('#section-workshop-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneWorkshop, '#section-workshop-child', getTopPercent());
+
+  sceneWorkshop.addTo(controller);
+
+  //-------------------------------------
+  // section-work
+  //-------------------------------------
+  var sceneWork = new ScrollMagic.Scene({
+    triggerElement: '#section-work-trigger',
+    duration: $('#section-work-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneWork, '#section-work-child', getTopPercent('', '10%'));
+
+  sceneWork.addTo(controller);
+
+  //-------------------------------------
+  // section-approach
+  //-------------------------------------
+  var sceneApproach = new ScrollMagic.Scene({
+    triggerElement: '#section-approach-trigger',
+    duration: $('#section-approach-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneApproach, '#section-approach-child', getTopPercent('0%', ''));
+
+  sceneApproach.addTo(controller);
+
+  //-------------------------------------
+  // section-about
+  //-------------------------------------
+  var sceneAbout = new ScrollMagic.Scene({
+    triggerElement: '#section-about-trigger',
+    duration: $('#section-about-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneAbout, '#section-about-child', getTopPercent());
+
+  sceneAbout.addTo(controller);
+
+  //-------------------------------------
+  // section-campfire
+  //-------------------------------------
+  var sceneCampfire = new ScrollMagic.Scene({
+    triggerElement: '#section-campfire-trigger',
+    duration: $('#section-campfire-trigger').height()
+  });
+
+  addEnterLeaveTransition(sceneCampfire, '#section-campfire-child', getTopPercent());
+
+  sceneCampfire.addTo(controller);
+}
+
+/*---------------------------------------------------------------------------*\
+ * Scroll-to-bottom
+\*---------------------------------------------------------------------------*/
+var scrollToBottom = function () {
+  $('html, body').scrollTop( $(document).height() );
+  $('body').css('animation-play-state', 'running');
+};
+
+$('body').on('animationend', function() {
+  $(this).css('opacity', 1);
 });
 
-addEnterLeaveTransition(sceneContact, '#section-contact-child', getTopPercent());
+/*---------------------------------------------------------------------------*\
+ * Scroll Up Button
+\*---------------------------------------------------------------------------*/
+$(document).scroll(document_scrollHandler);
+$(window).resize(document_scrollHandler.bind(window, true));
 
-sceneContact.addTo(controller);
+var loaded = false;
+$(document).ready(function () {
+  scrollToBottom();
 
-//-------------------------------------
-// section-story-type
-//-------------------------------------
-var sceneStoryType = new ScrollMagic.Scene({
-  triggerElement: '#section-story-type-trigger',
-  duration: $('#section-story-type-trigger').height()
+  setTimeout(function () {
+    loaded = true;
+    setupScenes();
+    $(document.body).addClass('body-fadein');
+  }, 500);
 });
 
-addEnterLeaveTransition(sceneStoryType, '#section-story-type-child', getTopPercent());
+function document_scrollHandler(instant) {
+  var instantaneous = instant || !loaded;
 
-sceneStoryType.addTo(controller);
+  var divScrollUp = $('.scroll-up');
+  var divSocialIcons = $('.social-fixed');
+  var divNarratorLogo = $('.narrator-logo-full');
 
-//-------------------------------------
-// section-workshop
-//-------------------------------------
-var sceneWorkshop = new ScrollMagic.Scene({
-  triggerElement: '#section-workshop-trigger',
-  duration: $('#section-workshop-trigger').height()
-});
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    divScrollUp.addClass('scroll-up-show');
+    divSocialIcons.addClass('social-show');
+    divNarratorLogo.addClass('narrator-logo-show');
+  }
+  else {
+    divScrollUp.removeClass('scroll-up-show');
+    divSocialIcons.removeClass('social-show');
+    divNarratorLogo.removeClass('narrator-logo-show');
+  }
 
-addEnterLeaveTransition(sceneWorkshop, '#section-workshop-child', getTopPercent());
-
-sceneWorkshop.addTo(controller);
-
-//-------------------------------------
-// section-work
-//-------------------------------------
-var sceneWork = new ScrollMagic.Scene({
-  triggerElement: '#section-work-trigger',
-  duration: $('#section-work-trigger').height()
-});
-
-addEnterLeaveTransition(sceneWork, '#section-work-child', getTopPercent('', '10%'));
-
-sceneWork.addTo(controller);
-
-//-------------------------------------
-// section-approach
-//-------------------------------------
-var sceneApproach = new ScrollMagic.Scene({
-  triggerElement: '#section-approach-trigger',
-  duration: $('#section-approach-trigger').height()
-});
-
-addEnterLeaveTransition(sceneApproach, '#section-approach-child', getTopPercent('0%', ''));
-
-sceneApproach.addTo(controller);
-
-//-------------------------------------
-// section-about
-//-------------------------------------
-var sceneAbout = new ScrollMagic.Scene({
-  triggerElement: '#section-about-trigger',
-  duration: $('#section-about-trigger').height()
-});
-
-addEnterLeaveTransition(sceneAbout, '#section-about-child', getTopPercent());
-
-sceneAbout.addTo(controller);
-
-//-------------------------------------
-// section-campfire
-//-------------------------------------
-var sceneCampfire = new ScrollMagic.Scene({
-  triggerElement: '#section-campfire-trigger',
-  duration: $('#section-campfire-trigger').height()
-});
-
-addEnterLeaveTransition(sceneCampfire, '#section-campfire-child', getTopPercent());
-
-sceneCampfire.addTo(controller);
-
-$(window).scroll(function () {
-  window.emberOptions.debugText = ' scroll:' + controller.scrollPos();
-
-  TweenLite.to('#campfire-video', 1, {
+  TweenLite.to('#campfire-video', instantaneous ? 0 : 1, {
     top: (documentHeight() - window.innerHeight - controller.scrollPos()) * window.CAMPFIRE_SCROLL_RATIO + (window.innerHeight - $('#campfire-video').height()),
     overwrite: 'concurrent',
     ease: Power1.easeOut
   });
-  TweenLite.to('#mountains-image', 1, {
+
+  TweenLite.to('#mountains-image', instantaneous ? 0 : 1, {
     top: (documentHeight() - window.innerHeight - controller.scrollPos()) * window.CAMPFIRE_SCROLL_RATIO + (window.innerHeight - $('#mountains-image').height()),
     overwrite: 'concurrent',
     ease: Power1.easeOut
   });
-});
+};
